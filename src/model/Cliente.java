8541fafff01467f22738a1a52be5386441bff27c
layssalana <layssas.lana@uni9.edu.br>
2025-05-19 17:01:45 -0300

    Update and rename README.md to src/model/Cliente.java

---
+++ src/model/Cliente.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Cliente {
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Cliente(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getSenha() { return senha; }
+}
